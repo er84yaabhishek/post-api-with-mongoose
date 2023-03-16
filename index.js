5a5ddac51539cb37739cb0ee4eca57eac59c5a58
er84yaabhishek <er84yaabhishek@gmail.com>
2023-03-16 14:18:23 +0530

    search api with nodejs

--- index.js
+++ index.js
@@ -4,27 +4,15 @@ const Product = require("./product");
 
 const app = express();
 app.use(express.json());
-app.post("/create", async (req, resp) => {
-  let data = new Product(req.body);
-  let result = await data.save();
-  console.log(result);
-  resp.send(result);
-});
-
-app.get("/list", async (req, resp) => {
-  let data = await Product.find();
-  resp.send(data);
-});
-app.delete("/delete/:_id", async (req, resp) => {
-  console.log(req.params);
-  let data = await Product.deleteOne(req.params);
-  resp.send(data);
-});
-app.put("/update/:_id", async (req, resp) => {
-  console.log(req.params);
-  let data = await Product.updateOne(req.params, {
-    $set: req.body,
+app.get("/search/:key", async (req, resp) => {
+  console.log(req.params.key);
+  let data = await Product.find({
+    $or: [
+      { name: { $regex: req.params.key } },
+      { brand: { $regex: req.params.key } },
+      { category: { $regex: req.params.key } },
+    ],
   });
   resp.send(data);
 });
-app.listen(5000);
+app.listen(8000);
